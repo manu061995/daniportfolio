646286bf41d752dd1e9e35b256361507e1cda282
manolo <nolodelatorre@gmail.com>
2018-02-27 20:38:16 +0100

    con media y arreglo en html

---
+++ Daniela_Silva/media/js/sketch.js
@@ -0,0 +1,19 @@
+
+function preload(){
+	
+
+}
+function setup()
+{
+var myCanvas = createCanvas(window.innerWidth, window.innerHeight);
+myCanvas.parent('myContainer');
+background(255);
+textSize(128);
+fill(0);
+noStroke();
+text('portfolio',182,200)
+console.log(text('portfolio',182,200))
+}
+function draw(){
+	
+}
